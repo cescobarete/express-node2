85587f20ed6e859137971ff6ded8d8683f12f60d
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-23 16:18:10 -0500

    Login system works check it out

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const app = express()
 const port = 3000
 
 // static Files
-app.use(express.static('public'))
-app.use('/html',express.static(__dirname + '/html'))
-app.use('/js',express.static(__dirname + '/js'))
-
-app.get('', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+app.use(express.static('./public'))
+//app.use('/course',express.static(__dirname + '/course'))
+//app.use('/regexp',express.static(__dirname + '/regexp'))
+//app.use('/landing',express.static(__dirname + '/landing'))
+//app.use('/dice',express.static(__dirname + '/dice'))
 
 // Listeen on port 3000
 app.listen(port, () => console.info('Listening on port ${port}'))
